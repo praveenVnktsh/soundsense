243df81c7665d8c90bbc3c3c06738299e040a450
praveenVnktsh <pveenvenkatesh@gmail.com>
2024-02-22 17:18:13 -0500

    updated an example

--- process_action_cam/frame_audio.py
+++ process_action_cam/frame_audio.py
@@ -1,13 +1,17 @@
 import os
 import sys
-import cv2
 import subprocess
+import ffmpeg
+import datetime
+import numpy as np
+
 
 ## for the same timestamp capture the audio and frame from a video and save it in two different folders using ffmpeg
 ## the audio and the frame should have the same name
-
-audio_dir = "data/run1/audio"
-frame_dir = "data/run1/frames"
+root = '../data/'
+run_id = 'run1' + '/'
+audio_dir = root + run_id + "audio/"
+frame_dir = root + run_id + "frames/"
 
 if not os.path.exists(audio_dir):
     os.makedirs(audio_dir)
@@ -15,26 +19,39 @@ if not os.path.exists(audio_dir):
 if not os.path.exists(frame_dir):
     os.makedirs(frame_dir)
 
-video_file = "data/run1/video.mp4"
+video_file = root + run_id + "video.mp4"
 
 ## extract timestamps
+# probe = ffmpeg.probe(video_file)
+# metadata = probe["format"]["tags"]
+# iso_time = metadata["creation_time"]
+# dt_object = datetime.datetime.strptime(iso_time, "%Y-%m-%dT%H:%M:%S.%fZ")
+# unix_timestamp = int(dt_object.timestamp())
+# video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
+# duration =  float(video_info['duration'])
+# frame_rate = int(video_info['avg_frame_rate'].split('/')[0])/int(video_info['avg_frame_rate'].split('/')[1])
 
-vid_cap = cv2.VideoCapture(video_file)
-timestamp_arr = []
-while True:
-    res, frame = vid_cap.read()
-    if not res:
-        break
-    timestamp = vid_cap.get(cv2.CAP_PROP_POS_MSEC)
-    timestamp_arr.append(timestamp)
+
 
 # use ffmepeg to extract the audio and the frames
-
-for t in range(0, len(timestamp_arr), 9):
-    command = f"ffmpeg -i {video_file} -ss {timestamp_arr[t]/1000} -t 0.3 -vn {audio_dir}/{timestamp_arr[t]}.mp3"
+save_audio = True
+save_video = True
+freq_rate = 10
+leading_zeros = 5
+if save_video:
+    command = f'ffmpeg -i {video_file} -r {freq_rate} {frame_dir}/%0{leading_zeros}d.png'
     subprocess.call(command, shell=True)
 
-    command = f'ffmpeg -i {video_file} -ss {timestamp_arr[t]/1000} -t 0.3 -vf "fps=1" {frame_dir}/{timestamp_arr[t]}.jpg'
+# get total number of images
+tot_images = len(os.listdir(frame_dir))
+
+with open(root + run_id + 'timestamps.txt', 'w') as f:
+    for i in range(tot_images):
+        f.write(str(i/freq_rate) + '\tframes/' + str(i).zfill(leading_zeros) + '.png' + '\n')
+
+
+if save_audio:
+    command = f"ffmpeg -i {video_file} -vn {audio_dir}/audio.wav"
     subprocess.call(command, shell=True)
 
 ### how to take history
--- slammer/README.md
+++ slammer/README.md
@@ -1,6 +1,21 @@
 ### Run ORB-SLAM3
 
+Start the container
+
 ```bash
-sudo xhost +local:root && docker run --privileged --name orb-3-container --rm -p 8087:8087 -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev:/dev:ro --gpus all -it lmwafer/orb-slam-3-ready:1.0-ubuntu18.04
+sudo xhost +local:root && \
+docker run --privileged --name orb-3-container \
+--rm -p 8087:8087 \
+-e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v /dev:/dev:ro \
+-v /home/praveen/dev/mmml/soundsense/data/:/dpds/data \
+--gpus all \
+-it lmwafer/orb-slam-3-ready:1.0-ubuntu18.04
 ```
 
+Run the code:
+
+```bash
+./Examples/Monocular/mono_tum Vocabulary/ORBvoc.txt 
+```
