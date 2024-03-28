af2627ea67180d38e19cf442cd753a5f080ec608
praveenVnktsh <pveenvenkatesh@gmail.com>
2024-03-28 17:43:54 -0400

    update inference pipeline
    assume only 1 action at a time. Taking argmax and moving corresponding dimension according to limits

--- stretch/model_inference.py
+++ stretch/model_inference.py
@@ -3,29 +3,70 @@ import torch
 import stretch_body.robot
 import time
 import cv2
-from soundsense.baselines.cnn.model import LitModel
+import sys
+import copy
+# from soundsense.baselines.cnn.model import LitModel
+from models.baselines.cnnlstm.model import CNNLSTMWithResNetForActionPrediction
+from models.baselines.mulsa.src.inference import MULSAInference
+
+sys.path.append("")
+
+lookup = {
+    0: [1., 0.5, 0.5, 0.5, 0.5],
+    1: [0., 0.5, 0.5, 0.5, 0.5],
+    2: [0.5, 0., 0.5, 0.5, 0.5],
+    3: [0.5, 1., 0.5, 0.5, 0.5],
+    4: [0.5, 0.5, 1., 0.5, 0.5],
+    5: [0.5, 0.5, 0., 0.5, 0.5],
+    6: [0.5, 0.5, 0.5, 1., 0.5],
+    7: [0.5, 0.5, 0.5, 0., 0.5],
+    8: [0.5, 0.5, 0.5, 0.5, 0.],
+    9: [0.5, 0.5, 0.5, 0.5, 1.],
+    10: [0.5, 0.5, 0.5, 0.5, 0.5],
+}
 
 def convert_to_action(model, inp):
     # action_space = [del_extension, del_height, del_lateral, del_roll, del_gripper]
     limits = [
-        [-0.05, 0.05],
-        [-0.05, 0.05],
-        [-0.05, 0.05],
-        [-10*np.pi/180, 10 * np.pi/180],
-        [20, 100]
+        [-0.05, 0.05], # extension
+        [-0.05, 0.05], # base
+        [-50, 50],      # gripper
+        [-0.05, 0.05], # lift
+        [-10*np.pi/180, 10 * np.pi/180] # roll
     ]
-    outputs = model(inp) # 5x1
+    outputs = model(inp) # 11x1
+    action_idx = torch.argmax(outputs)
+    lookup_copy = copy.deepcopy(lookup)
+    outputs = lookup_copy[int(action_idx)]
+    print("before norm argmax:", int(action_idx), "output:",outputs)
+    print("lookup, lookup[id]",lookup,lookup[int(action_idx)])
     for i in range(len(limits)):
         outputs[i] = outputs[i] * (limits[i][1] - limits[i][0]) + limits[i][0]
-
     return outputs
 
+# Model:
+# extension+, extension-, base-, base+, gripper+, gripper-, lift+, lift-, roll-, roll+, no_action
+# Map to:
+# extension, base, gripper, lift, roll
 
+# 0: [1, 0.5, 0.5, 0.5, 0.5]
+# 1: [0, 0.5, 0.5, 0.5, 0.5]
+# 2: [0.5, 0, 0.5, 0.5, 0.5]
+# 3: [0.5, 1, 0.5, 0.5, 0.5]
+# 4: [0.5, 0.5, 1, 0.5, 0.5]
+# 5: [0.5, 0.5, 0, 0.5, 0.5]
+# 6: [0.5, 0.5, 0.5, 1, 0.5]
+# 7: [0.5, 0.5, 0.5, 0, 0.5]
+# 8: [0.5, 0.5, 0.5, 0.5, 0]
+# 9: [0.5, 0.5, 0.5, 0.5, 1]
+
+# 0.1, 0.2, 0.1, 0.3, -0.1, 0.3, 0.5, 0.3, 0.2, 0.2, 0.1
+# --------| --------| ---------| --------| --------| ---
 def get_image(cap):
     ret, frame = cap.read()
     if not ret:
         return None
-    return frame
+    return torch.tensor(np.expand_dims(np.array(frame).transpose(2, 0, 1), (0, 1))).float()
 
 
 def run_loop(model):
@@ -47,7 +88,7 @@ def run_loop(model):
             if frame is not None:
                 action = convert_to_action(model, frame)
                 execute_action(r, action)
-                print(action)
+                print("action:  ",action)
             else:
                 print("No frame")
                 is_run = False
@@ -56,12 +97,12 @@ def run_loop(model):
     cap.release()
 
 def execute_action(r: stretch_body.robot.Robot , action):
-    # action_space = [del_extension, del_height, del_lateral, del_roll, del_gripper]
+    # action = [extension, base, gripper, lift, roll]
     r.arm.move_by(action[0])
-    r.lift.move_by(action[1])
-    r.base.translate_by(action[2])
-    r.end_of_arm.move_by('wrist_roll', action[3])
-    r.end_of_arm.move_by('stretch_gripper', action[4])
+    r.lift.move_by(action[3])
+    r.base.translate_by(action[1])      
+    r.end_of_arm.move_by('wrist_roll', action[4])
+    r.end_of_arm.move_by('stretch_gripper', action[2])
     r.push_command()
     return True
 
@@ -79,7 +120,7 @@ if __name__ == "__main__":
             print("Exiting...")
             exit()
 
-    r.lift.move_to(0.7)
+    r.lift.move_to(0.9)
     r.arm.move_to(0.0)
     r.end_of_arm.move_to('wrist_yaw', 0.0)
     r.end_of_arm.move_to('wrist_pitch', 0.0)
@@ -91,6 +132,8 @@ if __name__ == "__main__":
     
 
 
-    model = LitModel.load_from_checkpoint("/home/punygod_admin/SoundSense/soundsense/baselines/cnn/lightning_logs/version_16/checkpoints/epoch=872-step=17460.ckpt")
+    # model = MULSAInference.load_from_checkpoint("/home/hello-robot/soundsense/soundsense/stretch/models/baselines/mulsa/03272024_205921_last.ckpt")
+    model = CNNLSTMWithResNetForActionPrediction.load_from_checkpoint("/home/hello-robot/soundsense/soundsense/stretch/models/baselines/cnnlstm/epoch=28-step=8352.ckpt")
+
     run_loop(model)
     r.stop()
