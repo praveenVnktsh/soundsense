8085306084a1c45a7638cc6cce685915ce57ded8
praveenVnktsh <pveenvenkatesh@gmail.com>
2024-04-13 18:03:46 -0400

    aux confs

--- models/baselines/mulsa/train_all.sh
+++ models/baselines/mulsa/train_all.sh
@@ -1,4 +1,4 @@
-python3 train.py "/home/punygod_admin/SoundSense/soundsense/models/baselines/mulsa/conf/imi/train_cnn_audio.yaml"
-python3 train.py "/home/punygod_admin/SoundSense/soundsense/models/baselines/mulsa/conf/imi/train_mha_audio.yaml"
-python3 train.py "/home/punygod_admin/SoundSense/soundsense/models/baselines/mulsa/conf/imi/train_cnn_uni.yaml"
-python3 train.py "/home/punygod_admin/SoundSense/soundsense/models/baselines/mulsa/conf/imi/train_mha_uni.yaml"
+python3 train.py "/home/punygod_admin/SoundSense/soundsense/models/baselines/mulsa/conf/aux/train_cnn_audio.yaml"
+python3 train.py "/home/punygod_admin/SoundSense/soundsense/models/baselines/mulsa/conf/aux/train_mha_audio.yaml"
+python3 train.py "/home/punygod_admin/SoundSense/soundsense/models/baselines/mulsa/conf/aux/train_cnn_uni.yaml"
+python3 train.py "/home/punygod_admin/SoundSense/soundsense/models/baselines/mulsa/conf/aux/train_mha_uni.yaml"
